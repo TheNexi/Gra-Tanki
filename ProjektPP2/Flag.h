3ba6b34390d4d1ee31c2b6210e8bfa3c5a6ee0d0
KLRAB <130837863+KLRAB@users.noreply.github.com>
2023-06-10 21:33:37 +0200

    Add files via upload

--- ProjektPP2/Flag.h
+++ ProjektPP2/Flag.h
@@ -1,3 +1,8 @@
+/**
+*
+*@file Flag.h
+*@brief Plik nag³ówkowy klasy Flag
+*/
 #pragma once
 
 #include <iostream>
@@ -9,27 +14,54 @@
 #include <SFML/Audio.hpp>
 #include <SFML/Network.hpp>
 using namespace std;
-
+/**
+*
+*@class Flag
+*
+*@brief Klasa reprezentuj¹ca flagê w grze.
+*/
 class Flag
 {
 private:
-	sf::Texture texture;//obiekt klasy texture z biblioteki sfml
-	sf::Sprite sprite;//obiekt klasy sprite z biblioteki sfml
+	sf::Texture texture;//obiekt klasy texture z biblioteki sfml /**< Obiekt klasy sf::Texture przechowuj¹cy teksturê flagi. */
+	sf::Sprite sprite;//obiekt klasy sprite z biblioteki sfml /**< Obiekt klasy sf::Sprite reprezentuj¹cy flagê. */
 
-	sf::Vector2f coord;//obiekt klasy Vector2f z biblioteki sfml
-
+	sf::Vector2f coord;//obiekt klasy Vector2f z biblioteki sfml /**< Obiekt klasy sf::Vector2f przechowuj¹cy wspó³rzêdne flagi. */
+	/**
+   * @brief Inicjalizuje teksturê flagi.
+   */
 	void initTexture();
+	/**
+	 * @brief Inicjalizuje sprite flagi.
+	 */
 	void initSprite();
 
 public:
+	/**
+	* @brief Konstruktor klasy Flag.
+	*/
 	Flag();
+	/**
+	 * @brief Destruktor klasy Flag.
+	 */
 	virtual ~Flag();
-
+	/**
+	* @brief Ustawia pozycjê flagi na ekranie.
+	* @param pos_x Wspó³rzêdna x pozycji flagi.
+	* @param pos_y Wspó³rzêdna y pozycji flagi.
+	*/
 	void setPosition(float pos_x, float pos_y);
-
+	/**
+	* @brief Metoda zwracaj¹ca granice flagi.
+	* @return Obiekt klasy sf::FloatRect reprezentuj¹cy granice flagi.
+	*/
 	const sf::FloatRect getBounds() const; ////Metoda do granic obiektu(prostokatu)
 
 	float destructionHp = 5;
+	/**
+	* @brief Metoda renderuj¹ca flagê na oknie.
+	* @param window Referencja do obiektu klasy sf::RenderWindow, reprezentuj¹cego okno aplikacji.
+	*/
 	void render(sf::RenderWindow& window);
 
 };
