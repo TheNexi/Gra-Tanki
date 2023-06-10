674a81a87809faeea974b2cf46901577d2ef0a45
KLRAB <130837863+KLRAB@users.noreply.github.com>
2023-06-10 19:44:12 +0200

    Add files via upload

--- ProjektPP2/Bricks.cpp
+++ ProjektPP2/Bricks.cpp
@@ -1,9 +1,21 @@
+/**
+ * @file Bricks.cpp
+ * @brief Plik nag³ówkowy klasy Bricks
+ */
 #include "Bricks.h"
 #include <iostream>
 using namespace std;
 
 #include <SFML/Graphics.hpp>
-
+/**
+ * @brief Konstruktor klasy Bricks.
+ * Inicjalizuje obiekt ceg³y z przekazanymi parametrami.
+ *
+ * @param texture WskaŸnik do tekstury ceg³y.
+ * @param pos_x Wspó³rzêdna x pozycji ceg³y.
+ * @param pos_y Wspó³rzêdna y pozycji ceg³y.
+ * @param destructionHp Punkty zniszczenia ceg³y.
+ */
 Bricks::Bricks(sf::Texture* texture,float pos_x, float pos_y, float destructionHp)
 {
 	this->shape.setTexture(*texture); //tekstura obrazu pocisku
@@ -11,25 +23,37 @@ Bricks::Bricks(sf::Texture* texture,float pos_x, float pos_y, float destructionH
 	//this->shape.setOrigin(25, 25);
 	this->shape.setPosition(pos_x, pos_y); // ustawienie pozycji cegly na ekranie
 }
-
+/**
+ * @brief Destruktor klasy Bricks.
+ * Nie s¹ podejmowane ¿adne dzia³ania specyficzne dla destruktora klasy Bricks.
+ */
 Bricks::~Bricks()
 {
 
 }
 
-
+/**
+ * @brief Metoda zwracaj¹ca granice ceg³y.
+ * @return Obiekt klasy sf::FloatRect reprezentuj¹cy granice ceg³y.
+ */
 const sf::FloatRect Bricks::getBounds() const
 {
 	return this->shape.getGlobalBounds(); //Zwraca granice obiektu uwzglêdniaj¹c¹ pozycjê pocisku na ekranie
 }
 
-
+/**
+ * @brief Metoda aktualizuj¹ca stan ceg³y.
+ * Nie s¹ podejmowane ¿adne dzia³ania w metodzie update().
+ */
 void Bricks::update()
 {
 
 }
 
-
+/**
+ * @brief Metoda renderuj¹ca ceg³ê na oknie.
+ * @param window Referencja do obiektu klasy sf::RenderWindow, reprezentuj¹cego okno aplikacji.
+ */
 void Bricks::render(sf::RenderWindow& window)
 {
 	window.draw(shape); // Wyœwietlanie ceg³y na ekranie
--- ProjektPP2/Flag.cpp
+++ ProjektPP2/Flag.cpp
@@ -1,5 +1,16 @@
-#include "Flag.h"
+/**
+ * @file Flag.cpp
+ *
+ * @brief Plik nag³ówkowy klasy Flag.
+ */
 
+#include "Flag.h"
+ /**
+	  * @brief Inicjalizuje teksturê flagi.
+	  *
+	  * £aduje teksturê z pliku "Textures/flag1.jpg" i przypisuje j¹ do zmiennej texture.
+	  * W przypadku b³êdu podczas wczytywania wyœwietla odpowiednie komunikaty.
+	  */
 void Flag::initTexture()
 {
 	if (!this->texture.loadFromFile("Textures/flag1.jpg"))
@@ -7,36 +18,59 @@ void Flag::initTexture()
 		cout << "ERROR::PLAYER::Blad przy wczytywaniu grafiki flagi" << endl;
 	}
 }
-
+/**
+	 * @brief Inicjalizuje sprite flagi.
+	 *
+	 * Ustawia teksturê sprite'a na wczeœniej zainicjalizowan¹ teksturê.
+	 */
 void Flag::initSprite()
 {
 	//inicjacja tekstury do obiektu typu sprite
 	this->sprite.setTexture(this->texture);
 
 }
-
+/**
+	 * @brief Konstruktor klasy Flag.
+	 *
+	 * Inicjalizuje teksturê i sprite flagi poprzez wywo³anie funkcji initTexture() i initSprite().
+	 */
 Flag::Flag()
 {
 	this->initTexture();
 	this->initSprite();
 }
-
+/**
+	 * @brief Destruktor klasy Flag.
+	 */
 Flag::~Flag()
 {
 
 }
-
+/**
+	 * @brief Ustawia pozycjê flagi.
+	 *
+	 * @param x Wspó³rzêdna x pozycji flagi.
+	 * @param y Wspó³rzêdna y pozycji flagi.
+	 */
 void Flag::setPosition(const float x, const float y)//ustawia pozycje poprzed dwie zmienne
 {
 	this->sprite.setPosition(x, y);
 
 }
-
+/**
+	 * @brief Zwraca granice flagi.
+	 *
+	 * @return Granice flagi uwzglêdniaj¹ce jej pozycjê na ekranie.
+	 */
 const sf::FloatRect Flag::getBounds() const
 {
 	return this->sprite.getGlobalBounds(); //Zwraca granice obiektu uwzglêdniaj¹c¹ pozycjê pocisku na ekranie
 }
-
+/**
+	 * @brief Wyœwietla flagê na ekranie.
+	 *
+	 * @param window Referencja do okna renderowania, na którym ma byæ wyœwietlona flaga.
+	 */
 void Flag::render(sf::RenderWindow& window)
 {
 	window.draw(sprite); // Wyœwietlanie ceg³y na ekranie
